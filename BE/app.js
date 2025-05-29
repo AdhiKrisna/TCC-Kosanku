9d15380e924451d8107b4980b0b66711bfe64b3a
AdhiKrisna <krisnahmbtn@gmail.com>
2025-05-29 20:06:23 +0700

    Repairing port

--- BE/app.js
+++ BE/app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 association();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
