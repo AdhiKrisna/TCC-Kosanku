d87ae69753d177d12be4a00b2c2cf073c3baac41
AdhiKrisna <krisnahmbtn@gmail.com>
2025-05-29 20:44:01 +0700

    index to app

--- BE/app.js
+++ BE/app.js
@@ -36,6 +36,8 @@ const PORT = process.env.PORT || 5000;
 // const isProduction = process.env.NODE_ENV === "production";
 // console.log("isProduction:", isProduction);
 
-app.listen(PORT, () => {
+app.listen(
+    PORT, '0.0.0.0', 
+    () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
